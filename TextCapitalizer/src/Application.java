3c5b19a785e4c0e71cd151ef8d8d9c4fc0c96ef9
Bandara A.W.M.J.S. it15145994 <it15145994@my.sliit.lk>
2020-12-17 01:17:35 +0530

    Pushing Capitalization project

---
+++ TextCapitalizer/src/Application.java
@@ -0,0 +1,47 @@
+import java.awt.FileDialog;
+import java.awt.Frame;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Locale;
+
+public class Application {
+
+	public static void main(String[] args) throws IOException {
+		FileDialog fd = new FileDialog((Frame)null, "Select A Text File To Read", FileDialog.LOAD);
+	    fd.setVisible(true);
+	    String file = fd.getDirectory() + fd.getFile();
+	    System.out.println("Location :" + fd.getDirectory() + file);
+	    
+	    FileReader fr = null; 
+	    BufferedReader br = null;
+	    
+	    try {
+	    	
+			 fr = new FileReader(file);
+			 br = new BufferedReader(fr);
+			 String line = null;
+
+			while((line = br.readLine()) != null) {
+
+				line = line.toUpperCase();
+				System.out.println(line);
+				
+			}		
+			
+		} finally {
+			
+			if(br!=null) {
+				br.close();
+			}
+			if(fr!=null) {
+				fr.close();
+			}
+		}
+
+	    fd.dispose();
+
+	}
+
+}
